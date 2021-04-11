8345d4ac8cae98bfb4b6390f379610c34a5124d5
avinash7773 <javinash228@gmail.com>
2021-04-11 13:11:11 +0530

    Check condition of hours <=100 and days<=20

--- src/com/emp/Employee.java
+++ src/com/emp/Employee.java
@@ -2,40 +2,50 @@ package com.emp;
 
 public class Employee {
 	
-	static double is_employee_full_time = 2;
-	static double is_employee_part_time = 1;
-	static double is_employee_absent = 0;
 	static int wage_per_hours = 20;
-	static int hours;
+	static int total_hours = 0 ;
+	static  int working_days=0;
+	static int daily_hours=8;
+	double daily_wage;
 	
 	
-	int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+	
+	
 	
 	public void empAttendance() {
-		switch (empCheck) {
-			case 2 :
-				System.out.println("Employee is full time");
-				hours = 8;
-				break;
-			case 1 :
-				System.out.println("employee is part time");
-				hours = 4;
-				break;
 		
-			default :
-				System.out.println("Employee is absent");
-				hours = 0;
+		final int is_employee_full_time = 2;
+		final int is_employee_part_time = 1;
+		int days = 0;
+		int hours=0;
+		while(total_hours <= 100 && days <=20) {
+			int empCheck = (int) Math.floor(Math.random() *10) % 3;
+				switch (empCheck) {
+					case is_employee_full_time :
+						hours = 8;
+						days++;
+						break;
+					case is_employee_part_time :
+						hours = 4;
+						days++;
+						break;
+		
+					default :
+						hours = 0;
+						break;
+				}
+		total_hours = total_hours + hours;
+		working_days = working_days + days;
 		}
 	}
 	
 	public void dailyWage() {
-		double daily_wage = wage_per_hours * hours;
+		daily_wage = wage_per_hours * daily_hours;
 		System.out.println("Daily wage="+daily_wage);
 	}
 	
 	public void monthlyWage() {
-		int working_days=20;
-		double monthly_wage = wage_per_hours * (hours * working_days);
+		double monthly_wage = daily_wage  * working_days;
 		System.out.println("Monthly wage="+monthly_wage);
 	}
 	
