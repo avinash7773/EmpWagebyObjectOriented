c087c07e1852ff427122b39a28f1f067bb872496
avinash7773 <javinash228@gmail.com>
2021-04-11 11:32:50 +0530

    add part time employee and part time wage

--- src/com/emp/Employee.java
+++ src/com/emp/Employee.java
@@ -2,18 +2,23 @@ package com.emp;
 
 public class Employee {
 	
-	static double is_employee_present = 1;
+	static double is_employee_full_time = 2;
+	static double is_employee_part_time = 1;
 	static double is_employee_absent = 0;
 	static int wage_per_hours = 20;
 	static int hours;
 	
-	double empCheck = (int)Math.floor(Math.random() * 10) % 2;
+	double empCheck = (int)Math.floor(Math.random() * 10) % 3;
 	
 	public void empAttendance() {
-		if(empCheck == is_employee_present) {
-			System.out.println("Employee is present");
+		if(empCheck == is_employee_full_time) {
+			System.out.println("Employee is full time");
 			hours = 8;
-		}else {
+		}else if(empCheck ==  is_employee_part_time) {
+			System.out.println("employee is part time");
+			hours = 4;
+		}
+		else {
 			System.out.println("Employee is absent");
 			hours = 0;
 		}
